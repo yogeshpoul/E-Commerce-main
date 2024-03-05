64ed242b3864ad3936f196ed2bdd500306cab950
Yogesh Poul <96036197+yogeshpoul@users.noreply.github.com>
2024-03-05 14:12:40 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,5 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <App />
-    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8351897819873862"
-     crossorigin="anonymous"></script>
 );
 
